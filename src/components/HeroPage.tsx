5c77d55b5d5f4a911e2814aee4da386e16493ec0
VitalinaKorchova <vitalina.korchova@gmail.com>
2025-07-29 21:54:26 +0300

    heropage almost done

--- src/components/HeroPage.tsx
+++ src/components/HeroPage.tsx
@@ -1,3 +1,14 @@
+import {
+  FaBriefcase,
+  FaChartLine,
+  FaChartPie,
+  FaExchangeAlt,
+  FaNewspaper,
+  FaSatelliteDish,
+  FaUniversity,
+} from "react-icons/fa";
+import { HiOutlineArrowNarrowDown } from "react-icons/hi";
+
 export default function HeroPage() {
   return (
     <>
@@ -12,26 +23,74 @@ export default function HeroPage() {
             <span>SpaceV</span>
           </div>
           <div className="nav-link">
-            <ul className="nav-links-items">
-              <li>
-                <a>Home</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
-              <li>
-                <a>Benefits</a>
-              </li>
-              <li>
-                <a>Contact</a>
-              </li>
-              <li>
-                <a>Reviews</a>
-              </li>
-            </ul>
+            <div className="nav-links-items">
+              <ul>
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>About</a>
+                </li>
+                <li>
+                  <a>Benefits</a>
+                </li>
+                <li>
+                  <a>Contact</a>
+                </li>
+                <li>
+                  <a>Reviews</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
         <hr />
+
+        <div className="text-main">
+          <span>
+            Empower <span className="word-bold">Modern</span>
+          </span>
+          <span className="word-bold">Technology</span>
+        </div>
+
+        <div className="btn-wrapper">
+          <button>
+            <HiOutlineArrowNarrowDown />
+          </button>
+        </div>
+        <div className="list-companies-wrapper">
+          <div className="list-companies">
+            <div>
+              <FaSatelliteDish />
+              NovaLink
+            </div>
+            <div>
+              <FaChartLine />
+              Finora Capital
+            </div>
+            <div>
+              <FaExchangeAlt />
+              TradeNova Exchange
+            </div>
+            <div>
+              <FaUniversity />
+              EuroTrust Bank
+            </div>
+            <div>
+              <FaChartPie />
+              LedgerLogic
+            </div>
+            <div>
+              <FaNewspaper />
+              MacroPulse
+            </div>
+            <div>
+              <FaBriefcase />
+              Investora
+            </div>
+          </div>
+          <div className="fade"></div>
+        </div>
       </div>
     </>
   );
