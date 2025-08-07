53b248b70a809d60f22fde4cc239b9a8e64e6199
VitalinaKorchova <vitalina.korchova@gmail.com>
2025-08-07 19:28:58 +0300

    still going on develop  benefits section

--- src/components/BenefitsSection.tsx
+++ src/components/BenefitsSection.tsx
@@ -12,35 +12,38 @@ export default function BenefitsSection() {
           <span>Your Business Needs</span>
         </div>
         <div className="block-section">
-          <div className="block-lines-wrapper">
-            <div className="block-lines">
-              <div className="long-line"></div>
+          <div className="block-lines">
+            <div className="block-lines-top">
               <div className="circle-center-main">
                 <FaLayerGroup />
               </div>
             </div>
+            <div className="block-long-line"></div>
           </div>
-          <div className="block-for-circle-cards">
-            <div>
+          <div className="block-cards">
+            <div className="block-icons">
               <div>
                 <div className="circle-develop">
                   <FaRegLightbulb />
                 </div>
-                <div className="container-descr-develop"></div>
+                <div className="circle-technologies">
+                  <MdOutlineWebhook />
+                </div>
+                <div className="card-develop"></div>
+                <div className="card-technologies"></div>
               </div>
-              <div className="circle-technologies">
-                <MdOutlineWebhook />
+              <div>
+                <div className="circle-goals">
+                  <BsRocketTakeoffFill />
+                </div>
               </div>
             </div>
-            <div>
-              <div className="circle-goals">
-                <BsRocketTakeoffFill />
-              </div>
+            <div className="block-single-card">
+              <div className="bottom-card-goals"></div>
             </div>
           </div>
-
-          <div className="oval-left-shadow"></div>
-          <div className="oval-right-shadow"></div>
+          {/* <div className="oval-left-shadow"></div>
+          <div className="oval-right-shadow"></div> */}
         </div>
       </div>
     </>
